167efdd158db31600bd7a8136115d902cd98a886
Andrew Kurin <andrew_kurin@mail.ru>
2024-07-01 06:58:26 +0300

    second step

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from prettytable import PrettyTable
 
 from api_clients.hh_client import HHAPIClient
+from db_pack.config import config
+from db_pack.db_create import create_databases, create_schema, save_emp_to_database, save_vac_to_database
 
 
 def main():
-    list_company = ['сбер', 'альфа', 'VK']
+    '''#list_company = ['', 'альфа', 'VK', 'яндекс', 'мегафон', 'точка', '', 'газпром', 'мтс', 'авито']
+    list_company = ['ixora', 'альфа', 'VK', 'Хекслет', 'мегафон', 'точка', 'skypro', 'додо', 'мтс', 'авито']
     hh_client = HHAPIClient()
     for company in list_company:
         employers = hh_client.search_employers(company)
@@ -18,7 +21,7 @@ def main():
         print(table)
 
         #vacancies = hh_client.search_vacancies(78638)
-
+'''
     '''table = PrettyTable(field_names=['ID', 'Название компании', 'Ссылка', 'Кол-во вакансий'])
     table.sortby = 'Кол-во вакансий'
     table.reversesort = True
@@ -31,6 +34,29 @@ def main():
     #for vac in vacancies:
      #   print(vac)
     print(len(vacancies))
+'''
+
+    params = config()
+
+    create_databases('hh_api', params)
+    create_schema('hh_api', params)
+
+    list_company = ['ixora', 'позитив', 'VK', 'Хекслет', 'софтлайн', 'точка', 'skypro', 'додо', 'айтеко', 'авито']
+    hh_client = HHAPIClient()
+    for company in list_company:
+        employers = hh_client.search_employers(company)
+        max_zn = max([emp.open_vacancies for emp in employers])
+        for emp in employers:
+            if emp.open_vacancies == max_zn:
+                save_emp_to_database(emp, 'hh_api', params)
+                print(emp)
+                vacancies = hh_client.search_vacancies(emp.id)
+                save_vac_to_database(vacancies, 'hh_api', params)
+                break
+        '''save_emp_to_database(employers, 'hh_api', params)
+        for emp in employers:
+            vacancies = hh_client.search_vacancies(emp.id)
+            save_vac_to_database(vacancies, 'hh_api', params)
 '''
 
 if __name__ == '__main__':
