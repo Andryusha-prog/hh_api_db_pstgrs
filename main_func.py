7cad7f14dbc27d9399ecfabed71fa3cf5c9b68ef
Andrew Kurin <andrew_kurin@mail.ru>
2024-07-01 20:51:08 +0300

    finish?

--- main_func.py
+++ main_func.py
@@ -11,14 +11,19 @@ def first_step_function():
 
     list_company = ['ixora', 'позитив', 'VK', 'Хекслет', 'софтлайн', 'точка', 'skypro', 'додо', 'айтеко',
                     'авито']
+
     hh_client = HHAPIClient()
+
     for company in list_company:
         employers = hh_client.search_employers(company)
+        #выбирается только тот id компании, у которого больше всего вакансий
         max_zn = max([emp.open_vacancies for emp in employers])
         for emp in employers:
             if emp.open_vacancies == max_zn:
+                #сохраняется информация о работодателе
                 save_emp_to_database(emp, 'hh_api', params)
                 print(emp)
+                #сохраняются вакансии текущего работодателя
                 vacancies = hh_client.search_vacancies(emp.id)
                 save_vac_to_database(vacancies, 'hh_api', params)
                 break
